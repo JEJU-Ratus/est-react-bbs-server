96dbb50e7aebaaddaf05de8edcefc67e8f6b0383
jema <smh1141@naver.com>
2026-07-13 14:15:12 +0900

    feat : mysql 모듈 연결 및 비번 dotenv모듈로 숨기기

---
+++ README.md
@@ -0,0 +1,8 @@
+# 환경변수 설정
+
+.env 파일 생성
+
+DB_HOST=localhost
+DB_USER=root
+DB_PASSWORD=비밀번호
+DB_NAME=bbs
--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
 const port = 3000;
 
+// cors 허용
 let corsOptions = {
   origin: "*",
 };
 
 app.use(cors());
 
+// sql 연동
+dotenv.config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect();
+// 요청 주소에 따른 출력
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/list", (req, res) => {
+  const sqlQuery = "SELECT * FROM board";
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+/*
+db.query('Sql 쿼리', (err) => {
+  if (err) throw err;
+
+});
+*/
