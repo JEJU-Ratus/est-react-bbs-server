e54f955d3bbf4f4f2fe7ff9f62816a0d391309b0
jema <smh1141@naver.com>
2026-07-15 14:30:55 +0900

    feat : 게시글 이미지 업로드 기능 추가. 파일관리 multer 모듈 추가

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = express();
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
 const port = 3000;
+const multer = require("multer");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // json -> object
-
+app.use("/uploads", express.static("uploads")); // uploads 폴더 접근 권한
 // cors 허용
 let corsOptions = {
   origin: "*",
@@ -15,6 +16,20 @@ let corsOptions = {
 
 app.use(cors());
 
+// file 경로와 파일명
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    originExt = file.originalname.split(".")[1];
+    console.log(file.fieldname);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1000);
+    cb(null, uniqueSuffix + "-" + file.fieldname + "." + originExt);
+  },
+});
+
+const upload = multer({ storage: storage });
 // sql 연동
 dotenv.config();
 
@@ -43,18 +58,19 @@ app.get("/view", (req, res) => {
   // console.log(req.query);
   const id = req.query.id;
   const sqlQuery =
-    "SELECT title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board where id=?;";
+    "SELECT title, content, writer,image_path, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board where id=?;";
   db.query(sqlQuery, [id], (err, result) => {
     if (err) throw err;
     res.send(result);
   });
 });
-// create
-app.post("/write", (req, res) => {
+// create(write)
+app.post("/write", upload.single("image"), (req, res) => {
   console.log(req.body);
-  const { title, name, content } = req.body;
-  const sqlQuery = "INSERT INTO board (title, content, writer) values (?,?,?);";
-  db.query(sqlQuery, [title, content, name], (err, result) => {
+  const { title, writer, content } = req.body;
+  const imagePath = req.file ? req.file.path : null; // true면 업로드 된 파일의 경로
+  const sqlQuery = "INSERT INTO board (title, content, writer, image_path) values (?,?,?,?);";
+  db.query(sqlQuery, [title, content, writer, imagePath], (err, result) => {
     if (err) throw err;
     res.send(result);
   });
